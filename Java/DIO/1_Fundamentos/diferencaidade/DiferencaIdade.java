319c41a0fa2459fcd7e72ee320bff5ed2ca2403d
Guilherme Stelmach <guilhermestelmach@hotmail.com>
2026-08-30 12:12:07 -0300

    feat: Implements the 'Pessoa' class for Exercise 4 of DIO's Java Fundamentals course.

--- Java/DIO/1_Fundamentos/diferencaidade/DiferencaIdade.java
+++ Java/DIO/1_Fundamentos/diferencaidade/DiferencaIdade.java
@@ -2,21 +2,28 @@ package diferencaidade;
 
 import java.util.Scanner;
 
+class Pessoa {
+    String nome;
+    int idade;
+}
+
 public class DiferencaIdade {
     public void executar(Scanner scanner) {
+        Pessoa pessoa1 = new Pessoa();
         System.out.print("Digite o nome da primeira pessoa: ");
-        String nome1 = scanner.next();
+        pessoa1.nome = scanner.next();
 
         System.out.print("Digite a idade da primeira pessoa: ");
-        int idade1 = scanner.nextInt();
+        pessoa1.idade = scanner.nextInt();
 
+        Pessoa pessoa2 = new Pessoa();
         System.out.print("Digite o nome da segunda pessoa: ");
-        String nome2 = scanner.next();
+        pessoa2.nome = scanner.next();
 
         System.out.print("Digite a idade da segunda pessoa: ");
-        int idade2 = scanner.nextInt();
+        pessoa2.idade = scanner.nextInt();
 
-        int diferenca = Math.abs(idade1 - idade2);
-        System.out.println(nome1 + " e " + nome2 + " têm uma diferença de idade de " + diferenca + " anos.");
+        int diferenca = Math.abs(pessoa1.idade - pessoa2.idade);
+        System.out.println(pessoa1.nome + " e " + pessoa2.nome + " têm uma diferença de idade de " + diferenca + " anos.");
     }
 }
